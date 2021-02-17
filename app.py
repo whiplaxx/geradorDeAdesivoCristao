84a13f4bca2de654dd5c95a7dbb6de85dc659e1e
Whiplax <whiplaxx@gmail.com>
2021-02-17 16:26:50 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,64 @@
+#!/bin/env python3.7
+
+import editImage
+from os import listdir
+from PIL import Image, ImageDraw, ImageFont
+from random import choice, randint
+
+# PATHS
+IMAGES_PATH = 'images/'
+FIGURES_PATH = IMAGES_PATH+'figures/'
+FRAMES_PATH = IMAGES_PATH+'frames/'
+FONTS_PATH = 'fonts/'
+PHRASES_PATH = 'phrases.txt'
+
+# VARIABLES
+BORDER_SIZE_DEFAULT = (40,20,40,20)
+FONT_SIZE = 50
+ADESIVO_WIDTH = 300
+STROKE_WIDTH = 5
+STROKE_FILL = (0,0,0)
+
+if __name__ == "__main__":
+	
+	# Getting images names
+	ls_figuresNames = listdir(FIGURES_PATH)
+	ls_framesNames = listdir(FRAMES_PATH)
+	ls_fontsNames = listdir(FONTS_PATH)
+	ls_phrases = []
+	with open(PHRASES_PATH, 'r', encoding='utf-8') as phrasesFile:
+		for line in phrasesFile:
+			if line != '':
+				ls_phrases.append( line.replace('\\n', '\n'))
+	
+	# List of RGB colors for borders and fonts
+	ls_bordersColors = [ (255,255,255), (52,180,235), (237,128,170) ]
+	ls_fontsColors = [ (38,41,212), (242,5,52), (219,175,42)]
+
+	# Randomizing elements
+	figure = Image.open( FIGURES_PATH + choice(ls_figuresNames) )
+	frame = Image.open( FRAMES_PATH + choice(ls_framesNames) )
+	font = ImageFont.truetype( FONTS_PATH + choice(ls_fontsNames), FONT_SIZE)
+	fontColor = (randint(150, 200), randint(150, 200), randint(150, 200))
+	phrase = choice(ls_phrases)
+
+	# Adding border
+	adesivo = editImage.createBorder( figure, BORDER_SIZE_DEFAULT, choice(ls_bordersColors))
+	# Resizing frame
+	frame = editImage.resizeProp_width( frame, adesivo.size[0] )
+	# Adding the frames
+	adesivo = editImage.pasteBottom( adesivo, frame, 'l')
+	frame = frame.rotate(180)
+	adesivo = editImage.pasteTop( adesivo, frame, 'r')
+
+	# Resizing adesivo
+	adesivo = editImage.resizeProp_width(adesivo, ADESIVO_WIDTH)
+
+	# Adding text
+	draw = ImageDraw.Draw(adesivo)
+	textSize = draw.multiline_textsize(phrase, font=font, stroke_width=STROKE_WIDTH)
+	textPositionTuple = ( int((adesivo.size[0]-textSize[0])/2), int(adesivo.size[1]/5)*3 )
+	draw.multiline_text( textPositionTuple, phrase, font=font, fill=fontColor, align='center', stroke_width=STROKE_WIDTH, stroke_fill=STROKE_FILL)
+
+	adesivo.save('adesivo.png')
+
---
+++ editImage.py
@@ -0,0 +1,65 @@
+#!/bin/env python3.7
+
+from PIL import Image
+
+def resizeProp_width( image, newWidth ):
+	width = image.size[0]
+	height = image.size[1]
+	
+	newHeight = int( height * (newWidth / width) )
+	
+	newImage = image.resize( (newWidth, newHeight) )
+	return newImage
+
+def resizeProp_height( image, newHeight ):
+	width = image.size[0]
+	height = image.size[1]
+
+	newWidth = int( width * (newHeight / height) )
+	
+	newImage = image.resize( (newWidth, newHeight) )
+	return newImage
+
+def pasteTop( imageBase, imageFore, verticalAlignment='l'):
+	
+	if verticalAlignment=='r' or verticalAlignment=='right':
+		leftPosition = imageBase.size[0] - imageFore.size[0]
+	elif verticalAlignment=='c' or verticalAlignment=='center':
+		leftPosition = int( (imageBase.size[0] - imageFore.size[0])/ 2)
+	else:
+		leftPosition = 0
+	positionTuple = (leftPosition, 0)
+
+	imageBase.paste( imageFore, positionTuple, mask=imageFore)
+	return imageBase
+
+def pasteBottom( imageBase, imageFore, verticalAlignment='l'):
+	
+	if verticalAlignment=='r' or verticalAlignment=='right':
+		leftPosition = imageBase.size[0] - imageFore.size[0]
+	elif verticalAlignment=='c' or verticalAlignment=='center':
+		leftPosition = int( (imageBase.size[0] - imageFore.size[0])/ 2)
+	else:
+		leftPosition = 0
+	positionTuple = (leftPosition, imageBase.size[1]-imageFore.size[1])
+
+	imageBase.paste( imageFore, positionTuple, mask=imageFore)
+	return imageBase
+
+
+def createBorder( image, bordersSize, colorRgb=(255,255,255)):
+
+	newImageWidth = image.size[0]+bordersSize[0]+bordersSize[2]
+	newImageHeight = image.size[1]+bordersSize[1]+bordersSize[3]
+	newImageSize = ( newImageWidth, newImageHeight )
+
+	newImage = Image.new('RGB', newImageSize, colorRgb )
+	newImage.paste( image, ( bordersSize[0], bordersSize[1] ) )
+	return newImage
+
+
+
+
+
+
+
